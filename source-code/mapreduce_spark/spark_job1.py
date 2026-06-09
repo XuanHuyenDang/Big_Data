b1e81ccb947b17aa7b366186f09450d1674d91b5
Bùi Quốc Hậu <23110211@student.hcmute.edu.vn>
2026-06-09 11:16:47 +0700

    fix header

--- source-code/mapreduce_spark/spark_job1.py
+++ source-code/mapreduce_spark/spark_job1.py
@@ -1,13 +1,16 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import avg, col
 
-spark = SparkSession.builder.appName("Spark Job 1 - Temperature Trend Analysis").getOrCreate()
+spark = SparkSession.builder \
+    .appName("Spark Job 1 - Temperature Trend Analysis") \
+    .getOrCreate()
 
-raw_df = spark.read.csv("/user/hadoop/weather_data_input/part-m-00000", header=False, inferSchema=True)
-df = raw_df.toDF("weather_date", "province", "region", "temperature", "humidity", "precipitation", "wind_speed", "pressure", "weather_code", "source")
+df = spark.read.csv("/user/hadoop/weather_data_input/part-m-00000", header=True, inferSchema=True)
 
-trend_df = df.groupBy("weather_date").agg(avg("temperature").alias("avg_temperature")).orderBy("weather_date")
+trend_df = df.groupBy("weather_date") \
+             .agg(avg("temperature").alias("avg_temperature")) \
+             .orderBy("weather_date")
+
 trend_df.show(35)
 trend_df.write.mode("overwrite").csv("/user/hadoop/spark_output/temp_trend", header=True)
-
 spark.stop()
--- source-code/mapreduce_spark/spark_job2.py
+++ source-code/mapreduce_spark/spark_job2.py
@@ -3,10 +3,12 @@ from pyspark.sql.functions import sum, avg, col, desc
 
 spark = SparkSession.builder.appName("Spark Job 2 - Precipitation Analysis").getOrCreate()
 
-raw_df = spark.read.csv("/user/hadoop/weather_data_input/part-m-00000", header=False, inferSchema=True)
-df = raw_df.toDF("weather_date", "province", "region", "temperature", "humidity", "precipitation", "wind_speed", "pressure", "weather_code", "source")
+df = spark.read.csv("/user/hadoop/weather_data_input/part-m-00000", header=True, inferSchema=True)
 
-rain_analysis = df.groupBy("province", "region").agg(sum("precipitation").alias("total_precipitation"), avg("precipitation").alias("avg_precipitation")).orderBy(desc("total_precipitation"))
+rain_analysis = df.groupBy("province", "region").agg(
+    sum("precipitation").alias("total_precipitation"), 
+    avg("precipitation").alias("avg_precipitation")
+).orderBy(desc("total_precipitation"))
 rain_analysis.show(20)
 rain_analysis.write.mode("overwrite").csv("/user/hadoop/spark_output/rain_analysis", header=True)
 
