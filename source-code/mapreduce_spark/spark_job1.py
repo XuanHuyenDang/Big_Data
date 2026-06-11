2d6d001fb591e0299a5ab196a5a49729d027a7d7
Bùi Quốc Hậu <23110211@student.hcmute.edu.vn>
2026-06-11 20:00:27 +0700

    Update spark_job1.py

--- source-code/mapreduce_spark/spark_job1.py
+++ source-code/mapreduce_spark/spark_job1.py
@@ -1,16 +1,28 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import avg, col
+from pyspark.sql.functions import avg, month
 
-spark = SparkSession.builder \
-    .appName("Spark Job 1 - Temperature Trend Analysis") \
+spark = (
+    SparkSession.builder
+    .appName("Spark Job 1 - National Monthly Average Temperature")
     .getOrCreate()
+)
 
-df = spark.read.csv("/user/hadoop/weather_data_input/part-m-00000", header=True, inferSchema=True)
+df = spark.read.csv(
+    "/user/hadoop/weather_data_input/part-m-00000",
+    header=True,
+    inferSchema=True
+)
 
-trend_df = df.groupBy("weather_date") \
-             .agg(avg("temperature").alias("avg_temperature")) \
-             .orderBy("weather_date")
+trend_df = (
+    df.groupBy(month("weather_date").alias("month"))
+      .agg(avg("temperature").alias("avg_temperature"))
+      .orderBy("month")
+)
 
-trend_df.show(35)
-trend_df.write.mode("overwrite").csv("/user/hadoop/spark_output/temp_trend", header=True)
+
+trend_df.write.mode("overwrite").csv(
+    "/user/hadoop/spark_output/temp_trend",
+    header=True
+)
+
 spark.stop()
